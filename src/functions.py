12b406ff804b868f92a6c23a86d59e7047006ec3
Данил Чебанский <dan14n97@yandex.ru>
2024-05-11 02:47:36 +0300

    Added function for sorting vacancies by salary

--- src/functions.py
+++ src/functions.py
@@ -21,3 +21,12 @@ def get_vacancies_by_salary(filtered_list, min_value=20000, max_value=200000):
             if int(max_value) > vacancy["mid_salary"] > int(min_value):
                 got_list.append(vacancy)
         return got_list
+
+
+def sort_vacancies(got_list):
+    if len(got_list) == 0:
+        return []
+    else:
+        sorted_list = sorted(got_list, key=lambda x: x['mid_salary'], reverse=True)
+        return sorted_list
+    
