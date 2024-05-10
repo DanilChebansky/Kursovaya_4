c9be2c6de4318c47f89cce96f30e745d4494eab7
Данил Чебанский <dan14n97@yandex.ru>
2024-05-10 21:46:08 +0300

    Added class for working with vacancies and adding they to the list

--- src/classes.py
+++ src/classes.py
@@ -26,3 +26,48 @@ class HH(Parser):
             vacancies = response.json()['items']
             self.vacancies.extend(vacancies)
             self.params['page'] += 1
+
+
+class Vacancy:
+    """Класс для перевода полученных с сайта вакансий в вид списка словарей"""
+    @classmethod
+    def cast_to_object_list(cls, list_):
+        vacan_list = []
+        for vacanc in list_:
+            new_vacanc = cls(vacanc)
+            vacan_list.append(new_vacanc.__dict__())
+        return vacan_list
+
+    def __init__(self, vocabul):
+        self.name = vocabul["name"]
+        self.url = vocabul['apply_alternate_url']
+        self.salary = vocabul["salary"]
+        self.requirements = vocabul["snippet"]["requirement"]
+        self.experience = vocabul["experience"]["name"]
+        self.area = vocabul["area"]["name"]
+        if vocabul["salary"] is None:
+            self.salary = "Зарплата не указана"
+            self.mid_salary = 0
+        elif vocabul["salary"] is not None and vocabul["salary"]["from"] is None:
+            self.min_salary = 0
+            self.max_salary = vocabul["salary"]["to"]
+            self.mid_salary = (int(self.min_salary) + int(self.max_salary)) / 2
+        elif vocabul["salary"] is not None and vocabul["salary"]["to"] is None:
+            self.min_salary = vocabul["salary"]["from"]
+            self.max_salary = 0
+            self.mid_salary = vocabul["salary"]["from"]
+        else:
+            self.min_salary = vocabul["salary"]["from"]
+            self.max_salary = vocabul["salary"]["to"]
+            self.mid_salary = (int(self.min_salary) + int(self.max_salary)) / 2
+
+    def __dict__(self):
+        return {
+            "name": self.name,
+            "url": self.url,
+            "salary": self.salary,
+            "requirements": self.requirements,
+            "experience": self.experience,
+            "area": self.area,
+            "mid_salary": self.mid_salary
+        }
