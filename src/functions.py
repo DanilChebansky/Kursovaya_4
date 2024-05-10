a744de996c0a34fd613968ff5b84121d7eaca804
Данил Чебанский <dan14n97@yandex.ru>
2024-05-11 02:46:53 +0300

    Added function for filtering vacancies by salary

--- src/functions.py
+++ src/functions.py
@@ -11,3 +11,13 @@ def filter_vacancies(list_, words):
                     filtered_list.append(vacancy)
         return filtered_list
 
+
+def get_vacancies_by_salary(filtered_list, min_value=20000, max_value=200000):
+    got_list = []
+    if len(filtered_list) == 0:
+        return []
+    else:
+        for vacancy in filtered_list:
+            if int(max_value) > vacancy["mid_salary"] > int(min_value):
+                got_list.append(vacancy)
+        return got_list
